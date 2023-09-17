49f15e424caf2c41a65d599095821fbb192c490f
Murat Toprak <64306017+toprakmurat@users.noreply.github.com>
2023-09-17 14:45:37 +0300

    add blur method

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ import array
 import re
 import struct
 
+# Helper Functions
+
+# Convert 6 characters long hex value of a pixel to decimals
+# Then, save it to a dictionary called bgrDict
+# Sample input: '0d0e0f', Output: {0: 13, 1: 14, 2: 15}
 def saveBGR(BGRhex):
 	splitted = re.findall('..?', BGRhex)
+
 	bgrDict = {0: 0, 1: 0, 2: 0} # 0 is blue, 1 is green, 2 is red
 	for index, byte in enumerate(splitted):
 		value = int(byte, base=16)
@@ -11,8 +17,29 @@ def saveBGR(BGRhex):
 
 	return bgrDict
 
+# Return hex equivalent of decimal values up to 255
+def dec2hex(decimal):
+	if decimal > 255 or decimal < 0:
+		return None
 
-# Short information about Bitmap (sorry for wrong info, it is just a reminder for myself to understand in the future)
+	elif decimal < 16:
+		hexStr = str(0) + hex(decimal).lstrip('0x')
+
+	else:
+		hexStr = hex(decimal).lstrip('0x')
+	
+	return hexStr
+
+
+# Returns True for positive odd numbers otherwise returns False 
+def check_odd(number):
+	if number % 2 == 1 and number > 0:
+		return True
+
+	return False
+
+
+# Short information about Bitmap (sorry for wrong info, it is just a reminder for myself to understand Bitmap in the future)
 # Bitmap stores pixels' color values as BGR, Blue Green Red
 # First 14 Bytes are the first header, next 40 bytes are the second and last header
 # Byte 19-22 give the width, Byte 23-26 give you the height which is needed for calculation of total pixels
@@ -78,8 +105,73 @@ class Bitmap:
 		return pixels
 
 
+	# Grid size must be a positive odd number
+	def blur(self, grid_size = 3):
+		pixels = self.pixelData()
+		bytesPerPixel = self.bitsPerPixel // 8
+
+		# Split pixels list for every bytesPerPixel and store it in a new list called temp
+		# Example: [[149, 92, 1], [15, 20, 25], [38, 38, 38] ...]
+		temp = []
+		for i in range(0, len(pixels), bytesPerPixel):
+			temp.append(pixels[i:i + bytesPerPixel])
+
+		# Split temp for every row and store it in a new list called BGRpixels
+		# New list's length becomes self.height
+		# Example
+		# [
+		#	[[149, 92, 1], [15, 20, 25], [38, 38, 38] ...],
+		#	[[172, 72, 5], [25, 20, 15], [39, 39, 39] ...],
+		#	[[195, 52, 9], [45, 50, 55], [40, 40, 40] ...],
+		#	...
+		# ]
+		BGRpixels = []
+		for j in range(0, len(temp), self.width):
+			BGRpixels.append(temp[j:j + self.width])
+
+
+		if check_odd(grid_size):
+			distance = (grid_size - 1) // 2
+		else:
+			raise Exception('grid_size must be a positive odd number')
+
+		newPixels = []
+		for row_index, row in enumerate(BGRpixels):
+			for pixel_index, pixel in enumerate(row):
+				# Create a grid for every pixel
+				# Example : if grid_size == 3 then distance becomes 1
+				# create grid for indexes [0, 0, 0
+				#						   0, 1, 0
+				#						   0, 0, 0]
+				# 1 is center pixel which this grid is created for 0s are others
+				# creating this grid by adding values to center pixel's index 
+				# every number between (-1 * distance, distance)
+				# of course it must be checked there are no index 'overflows'
+				blurred = [0, 0, 0]
+				totalColors = [0, 0, 0]
+				counter = 0
+				for i in range(-1 * distance, distance + 1):
+					if i + row_index < 0 or i + row_index >= self.height:
+						continue
+
+					for j in range(-1 * distance, distance + 1):
+						if j + pixel_index < 0 or j + pixel_index >= self.width:
+							continue
+						counter += 1
+
+						for k in range(3):
+							totalColors[k] += BGRpixels[row_index + i][pixel_index + j][k]
+
+				for i in range(3):
+					blurred[i] = totalColors[i] // counter
+
+				newPixels.append(blurred)
+
+		return newPixels
+
 # Test
 # filepath = 'images/stadium.bmp'
 # with open(filepath, 'rb') as file:
 # 	myImage = Bitmap(file)
-# 	print(myImage.grayscale())
+# 	blurred = myImage.blur()
+# 	print(blurred)
